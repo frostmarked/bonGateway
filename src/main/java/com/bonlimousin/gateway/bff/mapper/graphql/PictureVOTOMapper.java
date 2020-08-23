d9dc5f62d13bbcc3f7e1d31d3cd4d57993ceede2
Johan Frostmark <johan@frostmark.com>
2020-08-23 20:47:12 +0200

    update graphql with new oas service for pictures

---
+++ src/main/java/com/bonlimousin/gateway/bff/mapper/graphql/PictureVOTOMapper.java
@@ -0,0 +1,32 @@
+package com.bonlimousin.gateway.bff.mapper.graphql;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Base64;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.bonlimousin.gateway.web.api.model.PictureVO;
+import com.bonlimousin.gateway.web.graphql.model.PictureVOTO;
+import com.bonlimousin.gateway.web.graphql.model.VisibilityTO;
+
+@Mapper
+public interface PictureVOTOMapper {
+	
+	PictureVOTOMapper INSTANCE = Mappers.getMapper(PictureVOTOMapper.class);
+
+	PictureVOTO voToTO(PictureVO vo);
+	
+	default VisibilityTO visibilityToVisibilityTO(PictureVO.VisibilityEnum visibilityEnum) {
+		return VisibilityTO.valueOf(visibilityEnum.getValue());
+	}
+	
+	default String offsetDateTimeToString(OffsetDateTime odt) {
+		return odt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"));
+	}
+	
+	default String bytesToString(byte[] bytes) {
+		return Base64.getEncoder().encodeToString(bytes);
+    }
+}
