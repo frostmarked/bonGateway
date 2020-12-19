e9ae1fe8c7b13b48137a6e34d4b8cc87b40c870a
Johan Frostmark <johan.frostmark@scientificmed.com>
2020-12-19 21:42:45 +0100

    [ci skip] ok, so no obvious plan. just trying to be creative
    but at least a few steps forward regarding LAF

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -3,5 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  today: number = Date.now();
+}
