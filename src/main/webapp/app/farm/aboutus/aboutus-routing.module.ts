55cba9fefc3ad82321417e0275a2030bdbbfcee5
Johan Frostmark <johan.frostmark@scientificmed.com>
2021-01-07 22:06:31 +0100

    custom bon logo
    swapped old log and other icons for custom
    and some text for landing page

--- src/main/webapp/app/farm/aboutus/aboutus-routing.module.ts
+++ src/main/webapp/app/farm/aboutus/aboutus-routing.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CompanyComponent } from './company/company.component';
 import { ContactComponent } from './contact/contact.component';
+import { Authority } from 'app/shared/constants/authority.constants';
+import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
 
 const routes: Routes = [
   {
     path: 'company',
     component: CompanyComponent,
+    data: {
+      authorities: [Authority.USER],
+    },
+    canActivate: [UserRouteAccessService],
   },
   {
     path: 'contact',
     component: ContactComponent,
+    data: {
+      authorities: [Authority.USER],
+    },
+    canActivate: [UserRouteAccessService],
   },
 ];
 
--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { VERSION } from 'app/app.constants';
 
 @Component({
   selector: 'jhi-footer',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
   today: number = Date.now();
+  version: string;
+
+  constructor() {
+    this.version = VERSION ? (VERSION.toLowerCase().startsWith('v') ? VERSION : 'v' + VERSION) : '';
+  }
 }
