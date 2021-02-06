c5a56f5e31d43289cf0ece409b84f5cebf2a03e7
Johan Frostmark <johan@frostmark.com>
2021-02-06 20:37:25 +0100

    thumbnail for each lineage
    cant decide how to handle lineage information etc
    so this will have to do for now

--- src/main/webapp/app/farm/cow/linage-list/linage-list.component.ts
+++ src/main/webapp/app/farm/cow/linage-list/linage-list.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FindLinagesGQL, FindCowPicturesGQL, PictureVo } from 'app/bonpublicgraphql/bonpublicgraphql';
+import {
+  FindLinagesGQL,
+  FindCowPicturesGQL,
+  PictureVo,
+  Visibility,
+  PictureSourceVo,
+  LinageVo,
+} from 'app/bonpublicgraphql/bonpublicgraphql';
 import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { CowService } from 'app/farm/cow/cow.service';
 
 interface LinageItemVM {
@@ -12,6 +19,21 @@ interface LinageItemVM {
   polled: boolean;
   picture$: Observable<PictureVo>;
 }
+
+const LINEAGE_EARTAGID_IMAGEURL_MAP = new Map([
+  [1, '/content/images/bon/lineage/1_europe/185_Europe-072003b.jpg'],
+  [2, '/content/images/bon/lineage/2_eglantine/2095_Eglantine-092004a.jpg'],
+  [3, '/content/images/bon/lineage/3_etincelle/176_Etincelle-072003b.jpg'],
+  [6, '/content/images/bon/lineage/6_esther/2127_Polled_Minerve-072003a.jpg'],
+  [7, '/content/images/bon/lineage/7_elyssee/75_Elysse-072004a.jpg'],
+  [8, '/content/images/bon/lineage/8_eure/131_Eure-072003b.jpg'],
+  [9, '/content/images/bon/lineage/9_emeraude/2099_Emeraude_ET-072003b.jpg'],
+  [30, '/content/images/bon/lineage/30_delphine/69_Juliette-072003a.jpg'],
+  [49, '/content/images/bon/lineage/49_epargne/154_Evita-082003b.jpg'],
+  [52, '/content/images/bon/lineage/52_estafette/2089_Elle-062005a.jpg'],
+  [98, '/content/images/bon/lineage/98_fanny/57_Fanny-082003a.jpg'],
+  [988, '/content/images/bon/lineage/988_uppee/2091_Uppee_ET-082003a.jpg'],
+]);
 
 @Component({
   selector: 'jhi-linage-list',
@@ -41,10 +63,33 @@ export class LinageListComponent implements OnInit {
                 familyname: linage!.familyname,
                 visibility: linage!.visibility,
                 polled: linage!.polled,
-                picture$: this.cowService.getFirstCowPicture(linage!.earTagId!),
+                picture$: this.getLineageImageUrl(linage!),
               } as LinageItemVM)
           )
         )
       );
+  }
+
+  private getLineageImageUrl(linage: LinageVo): Observable<PictureVo> {
+    if (!LINEAGE_EARTAGID_IMAGEURL_MAP.has(linage.earTagId!)) {
+      return this.cowService.getFirstCowPicture(linage.earTagId!);
+    }
+    const imgUrl = LINEAGE_EARTAGID_IMAGEURL_MAP.get(linage.earTagId!);
+    const pvo = {
+      id: 0,
+      caption: linage.familyname,
+      visibility: Visibility.RoleAnonymous,
+      taken: new Date().toISOString(),
+      sources: [
+        {
+          name: 'default-img',
+          url: imgUrl,
+          width: 400,
+          height: 300,
+          contentType: 'image/jpeg',
+        } as PictureSourceVo,
+      ],
+    } as PictureVo;
+    return of(pvo);
   }
 }
